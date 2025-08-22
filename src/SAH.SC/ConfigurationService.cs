f94cf0a14a4b3195a8d6b08e620dc957187f159e
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-08-22 23:02:04 +0300

    Update 1.1.0
    - Update SC version.
    - Update IConfigurationService

--- src/SAH.SC/ConfigurationService.cs
+++ src/SAH.SC/ConfigurationService.cs
@@ -3,6 +3,7 @@ using SC;
 using SC.Abstraction;
 using SC.Extensions;
 using Sugar.Object.Extensions;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -17,19 +18,21 @@ public sealed class ConfigurationService(IConfigurationFileResolver resolver, IC
     private const string ROOT_CONFIGURATION_NAME = "Root";
 
     /// <inheritdoc/>
-    public event ConfigurationRootSaveHandler OnRootSaved;
-
-    /// <inheritdoc/>
-    public event ConfigurationRootLoadHandler OnRootLoaded;
-
-    /// <inheritdoc/>
     public IConfigurationRoot Root { get; private set; }
 
     /// <inheritdoc/>
-    public bool IsLoaded { get; private set; }
+    public override int Order => (int)EServiceOrder.RootService;
 
     /// <inheritdoc/>
-    public override int Order => (int)EServiceOrder.RootService;
+    public IEnumerable<IConfigurationOption> LoadedOptions => Root.LoadedOptions;
+
+    /// <inheritdoc/>
+    public IConfigurationSettings Settings => Root.Settings;
+
+    IEnumerable<IReadOnlyConfigurationOption> IReadOnlyConfiguration.LoadedOptions => LoadedOptions;
+
+    /// <inheritdoc/>
+    public IEnumerable<IConfiguration> LoadedConfigurations => Root.LoadedConfigurations;
 
     /// <inheritdoc/>
     protected override void OnInitialized()
@@ -44,38 +47,52 @@ public sealed class ConfigurationService(IConfigurationFileResolver resolver, IC
 
         foreach(string filePath in Directory.GetFiles(CONFUIGURATIONS_PATH)) builder.Append(resolver.Resolve(filePath)).Forget();
 
-        var root = Root = builder.Build(ROOT_CONFIGURATION_NAME, settings);
+        Root = builder.Build(ROOT_CONFIGURATION_NAME, settings);
 
-        Task.Run(LoadConfigurationAsync).Forget();
+        Task.Run(this.LoadAsync).Forget();
     }
 
     /// <inheritdoc/>
-    protected override void OnDeinitialize() => Task.Run(SaveConfigurationAsync).Forget();
+    protected override void OnDeinitialize() => Task.Run(this.SaveAsync).Forget();
 
-    private async Task SaveConfigurationAsync()
-    {
-        var root = Root;
+    /// <inheritdoc/>
+    public bool HasOption(string path) => Root.HasOption(path);
 
-        if(root is not null)
-        {
-            await root.SaveAsync();
-            OnRootSaved?.Invoke(root);
-            Root = null;
-        }
+    /// <inheritdoc/>
+    public IEnumerable<string> GetOptionsNames(string path) => Root.GetOptionsNames(path);
 
-        IsLoaded = false;
-    }
+    /// <inheritdoc/>
+    public IConfigurationOption<T> GetOption<T>(string path) => Root.GetOption<T>(path);
 
-    private async Task LoadConfigurationAsync()
-    {
-        var root = Root;
+    /// <inheritdoc/>
+    public IConfigurationOption<T> AddOption<T>(string path, T value) => Root.AddOption(path, value);
 
-        if(root is not null)
-        {
-            await root.LoadAsync();
-            OnRootLoaded?.Invoke(root);
-        }
+    /// <inheritdoc/>
+    public void RemoveOption(string path) => Root.RemoveOption(path);
 
-        IsLoaded = true;
-    }
+    /// <inheritdoc/>
+    public bool HasConfiguration(string name) => Root.HasConfiguration(name);
+
+    /// <inheritdoc/>
+    public IConfiguration GetConfiguration(string name) => Root.GetConfiguration(name);
+
+    /// <inheritdoc/>
+    public void AddConfiguration(IConfiguration configuration) => Root.AddConfiguration(configuration);
+
+    /// <inheritdoc/>
+    public void RemoveConfiguration(string name) => Root.RemoveConfiguration(name);
+
+    /// <inheritdoc/>
+    public void Save(string path) => Root.Save(path);
+
+    /// <inheritdoc/>
+    public void Load(string path) => Root.Load(path);
+
+    /// <inheritdoc/>
+    public async Task SaveAsync(string path) => await Root.SaveAsync(path);
+
+    /// <inheritdoc/>
+    public async Task LoadAsync(string path) => await Root.LoadAsync(path);
+
+    IReadOnlyConfigurationOption<T> IReadOnlyConfiguration.GetOption<T>(string path) => GetOption<T>(path);
 }
